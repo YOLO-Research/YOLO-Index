2a720bab3ea8e5d87e4d15e32c66e3fd4df26a9b
bcillie <17112922+bcillie@users.noreply.github.com>
2020-07-21 21:51:29 -0400

    Fix timezone conversion

--- index.py
+++ index.py
@@ -44,8 +44,7 @@ def update_weights(file, date=time.time()):
     :type time: integer
     """
     conn = sqlite.create_connection(file)
-    comp = get_composition(conn, date - ((date + 14400) % 86400))
-    print(date - ((date + 14400) % 86400))
+    comp = get_composition(conn, date - ((date - 14400) % 86400))
     for c in comp:
         c["tim"] = date
     updates(conn, comp)
